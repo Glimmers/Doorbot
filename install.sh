98529abf2d8ca2dfb3563fb7fabed2a15e6d5c21
Kes Zerda <kes@h3m3.com>
2019-01-14 05:39:39 +0000

    Updated install.sh to install requisite python libraries

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ then
      return 1
 fi
 
+# Ensure the libraries we use are installed
+apt install python3-rpi.gpio python3-requests
+
 addgroup --system doorbot
 adduser --system --ingroup gpio doorbot
 cp doorbot.ini.example doorbot.py doorbot.service ringtest.py /home/doorbot
