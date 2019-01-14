408a3c4241d3c3835a606f895d8bf3a85fcfe0f3
Kes Zerda <kes@h3m3.com>
2019-01-14 21:20:14 +0000

    Added license text. Added README

--- install.sh
+++ install.sh
@@ -7,12 +7,19 @@ then
 fi
 
 # Ensure the libraries we use are installed
-apt install python3-rpi.gpio python3-requests
+apt install python3 python3-rpi.gpio python3-requests
 
 addgroup --system doorbot
 adduser --system --ingroup gpio doorbot
-cp doorbot.ini.example doorbot.py doorbot.service ringtest.py /home/doorbot
-ln /home/doorbot/doorbot.service /etc/systemd/system/
+
+for N in doorbot.ini.example doorbot.py doorbot.service ringtest.py
+    do cp $N /home/doorbot
+    chown doorbot:doorbot /home/doorbot/$N
+done
+
+if [ -f /etc/systemd/system/doorbot.service ]
+    then echo "Unit file already exists, skipping"
+    else ln /home/doorbot/doorbot.service /etc/systemd/system/
+fi
 systemctl daemon-reload
-systemctl enable doorbot.service
 
